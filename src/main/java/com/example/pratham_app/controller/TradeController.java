e74511ceb525fe40806e7adeb4d093507f187a37
Prathamesh Desai <115919176+pratham7512@users.noreply.github.com>
2025-01-30 16:59:32 +0530

    Update TradeController.java

--- src/main/java/com/example/pratham_app/controller/TradeController.java
+++ src/main/java/com/example/pratham_app/controller/TradeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.infosys.bond_tracking.model.Trade;
-import com.infosys.bond_tracking.service.TradeService;
+import com.example.pratham_app.Trade;
+import com.example.pratham_app.TradeService;
 
 @RestController
 @RequestMapping("/trades")
